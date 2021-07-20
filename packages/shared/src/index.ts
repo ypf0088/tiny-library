a9702378e08f0f2765f6b50589fd9ec6132b5b1f
plusy88 <yuepengfei@pwrd.com>
2021-07-20 18:56:19 +0800

    shared

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-20 15:57:52
+ * @LastEditTime: 2021-07-20 15:57:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /tiny-library/packages/shared/src/index.ts
+ */
