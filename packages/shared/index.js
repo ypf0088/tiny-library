4506b806e18e36f1186c6ae30e9679ada1ab8455
plusy88 <yuepengfei@pwrd.com>
2021-07-30 14:12:58 +0800

    update

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -2,7 +2,7 @@
  * @Author: yourname
  * @LastEditors: Please set LastEditors
  * @Date: 2021-07-22 11:08:38
- * @LastEditTime: 2021-07-22 14:48:01
+ * @LastEditTime: 2021-07-30 14:08:25
  * @FilePath: /packages/shared/index.js
  * @Description: file content
  * Copyright (C) 2021 yourname. All rights reserved.
@@ -10,7 +10,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/tiny-shared.cjs.js')
+  module.exports = require('./dist/shared.cjs.js')
 } else {
-  module.exports = require('./dist/tiny-shared.cjs.js')
+  module.exports = require('./dist/shared.cjs.js')
 }
--- packages/utils/index.js
+++ packages/utils/index.js
@@ -2,7 +2,7 @@
  * @Author: yourname
  * @LastEditors: Please set LastEditors
  * @Date: 2021-07-22 11:08:38
- * @LastEditTime: 2021-07-22 14:48:12
+ * @LastEditTime: 2021-07-30 14:08:15
  * @FilePath: /packages/utils/index.js
  * @Description: file content
  * Copyright (C) 2021 yourname. All rights reserved.
@@ -10,7 +10,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/tiny-utils.cjs.js')
+  module.exports = require('./dist/utils.cjs.js')
 } else {
-  module.exports = require('./dist/tiny-utils.cjs.js')
+  module.exports = require('./dist/utils.cjs.js')
 }
