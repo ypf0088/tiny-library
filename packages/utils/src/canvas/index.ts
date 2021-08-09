028293765cf88db71d4c208cf3a98b6d02c35a2e
plusy88 <yuepengfei@pwrd.com>
2021-08-09 10:45:02 +0800

    update

--- packages/utils/src/canvas/index.ts
+++ packages/utils/src/canvas/index.ts
@@ -2,29 +2,29 @@
  * @Author: yourname
  * @LastEditors: Please set LastEditors
  * @Date: 2021-08-05 15:56:23
- * @LastEditTime: 2021-08-05 16:06:46
+ * @LastEditTime: 2021-08-09 10:41:01
  * @FilePath: /packages/utils/src/canvas/index.ts
  * @Description: file content
  * Copyright (C) 2021 yourname. All rights reserved.
  */
 
-import { dataURLToBlob } from "../file";
+import { dataURLToBlob } from '../file';
 
 export const captureImage = (url: string) => {
     return new Promise(resolve => {
         const video = document.createElement('video');
-        video.autoplay = true
+        video.autoplay = true;
         video.src = url;
-        video.addEventListener('loadeddata', (e) => {
+        video.addEventListener('loadeddata', e => {
             const canvas = document.createElement('canvas');
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
             const context = canvas.getContext('2d');
-            if(!context) return;
+            if (!context) return;
             context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            const file = dataURLToBlob(canvas.toDataURL())
+            const file = dataURLToBlob(canvas.toDataURL());
             video.pause();
             resolve(file);
-        })
-    })
-}
+        });
+    });
+};
